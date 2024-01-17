80fb4a2f450d1558e74b9fc3ff249db0576a3df5
Pedro Alves <pedromacedo2603@outlook.com>
2024-01-17 10:08:17 -0300

    Melhorado ferramenta MoverArea para não sobrepor pixels vazios

--- js/moverArea-class.js
+++ js/moverArea-class.js
@@ -41,16 +41,28 @@ export class MoverArea {
             }
         });
 
-        document.getElementById("area-selecionada").addEventListener("click", () => { this.mover = true; });
-        document.getElementById("cancelar-mover").addEventListener("click", () => { this.mover = false; });
+        document.getElementById("area-selecionada").addEventListener("click", () => { this.mover = true; area.style.resize = "none";});
+        document.getElementById("cancelar-mover").addEventListener("click", () => { this.mover = false; area.style.resize = "both";});
     }
 
     MoverArea(ctx) {
         if (this.mover) {
             const areaMover = ctx.getImageData(this.posInicial.x, this.posInicial.y, this.posInicial.largura, this.posInicial.altura);
-            ctx.putImageData(areaMover, this.posFinal.x, this.posFinal.y);
+            const areaSobreposta = ctx.getImageData(this.posFinal.x, this.posFinal.y, this.posFinal.largura, this.posFinal.altura);
+            const areaFinal = new ImageData(this.posFinal.largura, this.posFinal.altura);
+            for (let i = 0; i < areaMover.data.length; i++) {
+                if (areaMover.data[i] !== 0) {
+                    areaFinal.data[i] = areaMover.data[i];
+                }
+                else {
+                    areaFinal.data[i] = areaSobreposta.data[i];
+                }
+            }
+
+            ctx.putImageData(areaFinal, this.posFinal.x, this.posFinal.y);
 
             this.mover = false;
+            document.getElementById("area-mover").style.resize = "both";
         }
     }
 }
